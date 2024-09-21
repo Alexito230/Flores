b47ee7864f0a60ff97941da78dcfaade5b2b0d19
Alexito230 <julgarcialopez14@hotmail.com>
2024-09-20 23:25:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 onload = () => {
         document.body.classList.remove("container");
-    
+        
         // Reproducir la música si no se ha iniciado previamente
         let bgMusic = document.getElementById("bgMusic");
-        let musicPlayed = localStorage.getItem("musicPlayed");
+        let isPlaying = localStorage.getItem("isPlaying");
     
-        if (!musicPlayed) {
+        // Continuar la música si ya estaba sonando en otra página
+        if (isPlaying === "true") {
             bgMusic.play();
-            localStorage.setItem("musicPlayed", "true");
         }
+    
+        // Detectar si el usuario hace clic para empezar la música (en caso de que autoplay esté bloqueado)
+        document.addEventListener("click", function() {
+            bgMusic.play();
+            localStorage.setItem("isPlaying", "true");
+        });
+    
+        // Guardar el estado de la música cuando el usuario sale de la página
+        window.onbeforeunload = function() {
+            if (!bgMusic.paused) {
+                localStorage.setItem("isPlaying", "true");
+            } else {
+                localStorage.setItem("isPlaying", "false");
+            }
+        };
     };
     
